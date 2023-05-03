1c1268f3b324bae3ea68923602e5787b99b24a9d
muchokiesther <muchokiesther8@gmail.com>
2023-05-03 22:09:23 +0300

    This adds functionality to register

---
+++ dist/appli.js
@@ -0,0 +1,51 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+class Users {
+    Showusers() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield fetch(' http://localhost:3000/users');
+            const users = yield response.json();
+            //console.log(users);
+        });
+    }
+}
+new Users().Showusers();
+class UserData {
+    adddata(input) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield fetch("http://localhost:3000/users", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(input),
+            });
+            const result = yield response.json();
+            console.log(result);
+        });
+    }
+}
+const addButton = document.querySelector("#signupbtn");
+addButton.addEventListener("click", () => {
+    const Usersemail = document.querySelector("#username1");
+    const Userfullname = document.querySelector("#username2");
+    const Username = document.querySelector("#username3");
+    const PasswordII = document.querySelector("#passwordII");
+    const password = document.querySelector("#password");
+    const input = {
+        Usersemail: Usersemail.value,
+        Userfullname: Userfullname.value,
+        Username: Username.value,
+        PasswordII: PasswordII.value,
+        password: password.value,
+    };
+    new UserData().adddata(input);
+});
---
+++ src/appli.ts
@@ -0,0 +1,53 @@
+interface Userinfo {
+  
+  Usersemail: string;
+  Userfullname: string;
+  Username: string;
+  PasswordII: string;
+  password: string;
+}
+
+
+class Users{
+    async Showusers(){
+    const response = await fetch (' http://localhost:3000/users');
+    const users = await response.json()
+  //console.log(users);
+
+    }
+}
+new Users().Showusers()
+
+class UserData{
+    async adddata(input: Userinfo) {
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(input),
+    });
+    const result = await response.json();
+    console.log(result);
+  }
+}
+
+const addButton = document.querySelector("#signupbtn") as HTMLButtonElement;
+addButton.addEventListener("click", ()=> {
+  const Usersemail= document.querySelector("#username1") as HTMLInputElement;
+  const Userfullname = document.querySelector("#username2") as HTMLInputElement;
+  const Username= document.querySelector("#username3") as HTMLInputElement;
+  const PasswordII = document.querySelector("#passwordII") as HTMLInputElement;
+  const password = document.querySelector("#password") as HTMLInputElement;
+  const input: Userinfo = {
+    
+    Usersemail:Usersemail.value,
+    Userfullname: Userfullname.value,
+    Username: Username.value,
+    PasswordII: PasswordII.value,
+    password: password.value,
+  };
+  new UserData().adddata(input);
+})
+
+
